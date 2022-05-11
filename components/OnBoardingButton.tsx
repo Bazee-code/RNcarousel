fa0b79db9374f5992c5692dfb20cdc14c205489d
Bazee-code <eugeneobazee2@gmail.com>
2022-05-11 13:32:59 +0300

    Style on boarding button

--- components/OnBoardingButton.tsx
+++ components/OnBoardingButton.tsx
@@ -1,11 +1,22 @@
 import { View, StyleSheet, Text, Animated} from 'react-native'
+import Svg, { G, Circle } from 'react-native-svg'
 
 const OnBoardingButton = () => {
-    
+    const size = 128
+    const strokeWidth = 2
+    const center = size / 2
+    const radius = size / 2 - strokeWidth / 2
+    const circumference = 2 * Math.PI * radius
 
     return (
-        <View>
-            <Text>On boarding button</Text>
+        <View style={styles.container}>
+            <Svg height={size} width={size}>
+                <G rotation={-90} origin={center}>
+                    <Circle cx={center} cy={center} r={radius} stroke="#E6E7EB" strokeWidth={strokeWidth} />
+                    <Circle cx={center} cy={center} r={radius} stroke="#00008B" strokeWidth={strokeWidth}
+                        strokeDasharray={circumference} strokeDashoffset={circumference - (circumference*25) / 100} />
+            </G>
+            </Svg>
         </View>
     )
 }
@@ -13,5 +24,9 @@ const OnBoardingButton = () => {
 export default OnBoardingButton
 
 const styles = StyleSheet.create({
-
+    container : {
+        flex : 1,
+        justifyContent : 'center',
+        alignItems : 'center'
+    }
 })
