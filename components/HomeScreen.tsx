6e3cee4d35684b3aea60cacde217aaf2d35d1341
Bazee-code <eugeneobazee2@gmail.com>
2022-05-11 17:08:50 +0300

    Create home screen component

---
+++ components/HomeScreen.tsx
@@ -0,0 +1,20 @@
+import { View, StyleSheet, Text } from 'react-native'
+
+const HomeScreen = () => {
+
+    return (
+        <View>
+            <Text>Home Screen</Text>
+        </View>
+    )
+}
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+    container : {
+        flex  : 1,
+        justifyContent : 'center',
+        alignItems : "center"
+    }
+})
