76a627187d03545a07add698eb8af2f9c26579d9
Bazee-code <eugeneobazee2@gmail.com>
2022-05-11 10:00:27 +0300

    Style title & description texts

--- components/OnBoardingItem.tsx
+++ components/OnBoardingItem.tsx
@@ -12,10 +12,13 @@ type slidesProps = {
 const OnBoardingItem = ({item}: slidesProps) => {
     const { width }  = useWindowDimensions()
 
-    // console.log('item',item)
     return(
         <View style={[styles.container, {width}]}>
             <Image source={item.image} style={[styles.image, {width, resizeMode: 'contain'}]} />
+            <View style={{flex:0.3}}>
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.description}>{item.description}</Text>
+            </View>
         </View>
     )
 }
@@ -29,6 +32,18 @@ const styles = StyleSheet.create({
         alignItems : 'center'
     },
     image : {
-
+        flex : 0.7,
+        justifyContent : 'center'
+    },
+    title : {
+        textAlign : 'center',
+        fontWeight : '800',
+        fontSize : 20
+    },
+    description : {
+        textAlign : 'center',
+        paddingHorizontal : 10,
+        paddingTop : 10,
+        color : 'grey'
     }
 })
