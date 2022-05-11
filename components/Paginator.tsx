46982350f65f54f91a5dfef1b485aee3363126ec
Bazee-code <eugeneobazee2@gmail.com>
2022-05-11 11:28:44 +0300

    Render pagination slider

--- components/Paginator.tsx
+++ components/Paginator.tsx
@@ -1,10 +1,19 @@
-import { View , Text, StyleSheet} from 'react-native'
+import { View , Text, StyleSheet, ImageSourcePropType} from 'react-native'
+import slides from '../assets/common/slides'
 
-const Paginator = () => {
+type paginatorProps = {
+    data : typeof slides
+}
 
+const Paginator = ({data} : paginatorProps) => {
+    
     return (
         <View style={styles.container}>
-            <Text>Paginator</Text>
+            {
+                data.map((item,index)=>(
+                    <View key={index.toString()} style={styles.dot} />
+                ))
+            }
         </View>
     )
 }
@@ -13,8 +22,14 @@ export default Paginator
 
 const styles = StyleSheet.create({
     container : {
-        flex : 1,
-        justifyContent : 'center',
-        alignItems : "center"
+        flexDirection : 'row',
+        height : 64
+    },
+    dot : {
+        height : 10,
+        width : 10,
+        borderRadius : 5,
+        backgroundColor : '#493d8a',
+        marginHorizontal : 8
     }
 })
