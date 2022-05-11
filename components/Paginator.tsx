6baacc1effaf194fdd577d45edc148dbbecabcc9
Bazee-code <eugeneobazee2@gmail.com>
2022-05-11 11:44:59 +0300

    Make current tab twice the size of the others

--- components/Paginator.tsx
+++ components/Paginator.tsx
@@ -1,18 +1,33 @@
-import { View , Text, StyleSheet, ImageSourcePropType} from 'react-native'
+import { View , StyleSheet, Animated, useWindowDimensions} from 'react-native'
 import slides from '../assets/common/slides'
 
 type paginatorProps = {
     data : typeof slides
 }
+type scrollProps = {
+    scrollX : Animated.Value
+}
 
-const Paginator = ({data} : paginatorProps) => {
-    
+const Paginator = ({data} : paginatorProps, {scrollX} : scrollProps) => {
+    const { width } = useWindowDimensions()
+
     return (
         <View style={styles.container}>
             {
-                data.map((item,index)=>(
-                    <View key={index.toString()} style={styles.dot} />
-                ))
+                data.map((item,index)=>{
+
+                    const inputRange = [(index-1)* width, index* width, (index+1)*width]
+                    
+                    const dotWidth = scrollX.interpolate({
+                        inputRange,
+                        outputRange : [10,20,10],
+                        extrapolate: 'clamp'
+                    })
+
+                    return(
+                        <View key={index.toString()} style={styles.dot} />
+                    )
+                })
             }
         </View>
     )
