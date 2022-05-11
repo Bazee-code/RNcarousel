7385343871326ad6fbd3d7aa8bebd1d912973b2a
Bazee-code <eugeneobazee2@gmail.com>
2022-05-11 08:24:05 +0300

    Create slides json file

---
+++ assets/common/slides.tsx
@@ -0,0 +1,26 @@
+export default [
+    {
+        id : '1',
+        title : 'Quick & Easy Payments',
+        description : 'Grow your business by accepting card payments with the new card reader.',
+        image : require('../images/fashion-blogging.png')
+    },
+    {
+        id : '2',
+        title : 'Smart Point of Sale',
+        description : 'Complete point of sale software tailored to your business needs.',
+        image : require('../images/social-thinking.png')
+    },
+    {
+        id : '3',
+        title : 'Instant notifications',
+        description : 'Instant notifications let you quickly see new purchases and messages.',
+        image : require('../images/mobile-encrypt.png')
+    },
+    {
+        id : '4',
+        title : 'Customize everything',
+        description : 'Adjust your system to speed up your checkout.',
+        image : require('../images/compose-music.png')
+    }
+]
