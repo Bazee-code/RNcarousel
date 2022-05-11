dd1676b5d177872cc79bea793e2ad7c45b375555
Bazee-code <eugeneobazee2@gmail.com>
2022-05-11 09:17:46 +0300

    Add image, render image styles

--- components/OnBoardingItem.tsx
+++ components/OnBoardingItem.tsx
@@ -10,12 +10,25 @@ type slidesProps = {
 }
 
 const OnBoardingItem = ({item}: slidesProps) => {
-    const { width : number } = useWindowDimensions()
+    const { width }  = useWindowDimensions()
 
-    console.log('item',item)
+    // console.log('item',item)
     return(
-        <View></View>
+        <View style={[styles.container, {width}]}>
+            <Image source={item.image} style={[styles.image, {width, resizeMode: 'contain'}]} />
+        </View>
     )
 }
 
 export default OnBoardingItem
+
+const styles = StyleSheet.create({
+    container : {
+        flex : 1,
+        justifyContent : 'center',
+        alignItems : 'center'
+    },
+    image : {
+
+    }
+})
